37a139b126533d71ae71d762bbbe5f8ede07e460
khaled Abdlhamid <124719279+Khaled-Abdlhamid@users.noreply.github.com>
2025-08-18 11:42:46 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 class Settings:
     AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
     AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
-    AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT", "")
-    AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION", "")
+    AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+    AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
 
 # Create an instance of the settings
 settings = Settings()
